4ced44d2f88f68984fd9384b799f33a356f3b2bb
Miguel Leite <mikleite@gmail.com>
2023-08-11 17:47:49 +0100

    config atlas connection

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/dogs4life";
+  "mongodb+srv://mikleite:UTj1xL3E1bEomanu@ironhack-webdev-bootcam.vmxtj0t.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGO_URI)
