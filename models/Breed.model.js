ed8cc23dd6f6e0693ba30d7dbfdb63cc8690ab31
Miguel Leite <mikleite@gmail.com>
2023-08-15 10:25:40 +0100

    changes to file structure

---
+++ models/Breed.model.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require("mongoose");
+
+const breed = new Schema(
+);
+
+const DogBreed = model("Breed", breed);
+
+module.exports = DogBreed;
---
+++ routes/dog.routes.js
@@ -0,0 +1,13 @@
+//Requiring Packages
+const express = require("express");
+const router = express.Router();
+const session = require("express-session");
+const isLoggedIn = require("../middleware/isLoggedIn");
+const isLoggedOut = require("../middleware/isLoggedOut");
+
+const Breed = require("../models/Breed.model");
+const User = require("../models/User.model");
+
+// Fetch dog breeds, save them to the database, and display them
+
+module.exports = router;
