652c24e4a84bb278278107e5ca37c7e95b9f8ce6
Miguel Leite <mikleite@gmail.com>
2023-08-15 17:13:47 +0100

    solve logout and add headers

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/User.model");
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  const currentUser = req.session.currentUser
-  res.render("index", currentUser);
+router.get("/", async (req, res, next) => {
+  try {
+    const currentUser = req.session.currentUser;
+
+    console.log(currentUser);
+    res.render("index", { currentUser });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
