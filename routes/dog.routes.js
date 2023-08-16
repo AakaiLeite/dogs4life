f1d7bc75b63763e1f8ac02d3351dc271648ed10a
Miguel Leite <mikleite@gmail.com>
2023-08-16 11:55:30 +0100

    add breed list page, edit css on headers

--- routes/dog.routes.js
+++ routes/dog.routes.js
@@ -12,7 +12,11 @@ const Breed = require("../models/Breed.model");
 const User = require("../models/User.model");
 
 // Routes
-router.get("/breeds", async (req, res) => {
+router.get("/breeds", (req, res) => {
+  res.render("breed-list");
+});
+
+router.get("/breeds/search", async (req, res) => {
   const currentUser = req.session.currentUser;
   try {
     const allBreeds = await Breed.find();
@@ -21,7 +25,7 @@ router.get("/breeds", async (req, res) => {
       currentUser: currentUser,
     });
   } catch (error) {
-    console.log('Erro Breed Find: ', error);
+    console.log("Erro Breed Find: ", error);
   }
 });
 
@@ -33,15 +37,19 @@ router.get("/breed/:breedId", async (req, res) => {
     let dogImg = await dogApi.getBreedImage(breed.id);
     dogImg = dogImg.data[0].url;
 
-    res.render("breed-details", { breed: breed, image: dogImg, currentUser: currentUser });
+    res.render("breed-details", {
+      breed: breed,
+      image: dogImg,
+      currentUser: currentUser,
+    });
   } catch (error) {
     console.log("Error Breed Details: ", error);
   }
 });
 
-router.get('/trending', (req, res) => {
+router.get("/trending", (req, res) => {
   const currentUser = req.session.currentUser;
-  res.render('breed-trending', {currentUser});
-})
+  res.render("breed-trending", { currentUser });
+});
 
 module.exports = router;
